d0c8293b4625f32472d0fbcd5674034a5f2e0909
ricardocoelho22 <ricardocoelho22@gmail.com>
2016-07-17 18:26:48 +0100

    complete events layout

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,13 @@
-$( document ).ready(function() {
+$(document)
+  .ready(function() {
 
-  var leftMenu = $("header.site-header nav a");
-  leftMenu.slice(0, Math.floor(leftMenu.length/2)).css("order", "2");
+    // var leftMenu = $("header.site-header nav a");
+    // leftMenu.slice(0, Math.floor(leftMenu.length/2)).css("order", "2");
+    // 
 
-});
+    var description = $('.event-container p.description');
+    description.dotdotdot();
+    var title = $('.event-container p.title');
+    title.dotdotdot();
+
+  });
