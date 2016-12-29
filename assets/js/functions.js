382d867be60989001e4e82c546e73e06f781ab9c
ricardocoelho22 <ricardocoelho22@gmail.com>
2016-12-29 23:54:41 +0000

    Added initial contact form js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,57 @@
+var googleMapsInfo = {
+  apiKey: 'AIzaSyCTYZoDbk3_Vc5t8uBKRMGR6UhjkjKgpWY',
+  callback: 'initMap',
+  coordSCA: { lat: 39.056272, lng: -9.006574 },
+  coordTitle: 'Sporting Clube de Alenquer'
+}
+
 function initMap() {
-  var coordSCA = { lat: 39.056272, lng: -9.006574 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: coordSCA
+    center: googleMapsInfo.coordSCA
   });
   var marker = new google.maps.Marker({
-    position: coordSCA,
+    position: googleMapsInfo.coordSCA,
     map: map,
-    title: "Sporting Clube de Alenquer"
+    title: googleMapsInfo.title
   });
+}
+
+function loadGoogleAPI() {
+  var head = document.getElementsByTagName('head')
+    .item(0);
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=' +
+    googleMapsInfo.apiKey + '&callback=' + googleMapsInfo.callback);
+  head.appendChild(script);
 }
 
 $(document)
   .ready(function() {
+    loadGoogleAPI();
 
+    $('#contact-form').submit(function(e) {
+      var name = $('#contact-name');
+      var email = $('#contact-email');
+      var subject = $('#contact-subject');
+      var message = $('#contact-message');
+
+      if (name.val() == "" || email.val() == "" || subject.val() == "" ||
+        message.val() == "") {
+        // $('.submit-fail')
+        //   .fadeToggle(400);
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: 'https://formspree.io/ricardocoelho22@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        // $('.submit-success').fadeToggle(400);
+      }
+    });
   });
